da620b84ba3c71ea8ce8b395aa8606653bf4577f
rin1004 <rin691004@naver.com>
2022-03-24 13:37:45 +0900

    last

--- lib/screens/alarm.dart
+++ lib/screens/alarm.dart
@@ -12,6 +12,7 @@ class Alarm extends StatefulWidget {
 class _AlarmState extends State<Alarm> {
   FlutterLocalNotificationsPlugin? _flutterLocalNotificationsPlugin;
   DateTime _dateTime = DateTime.now();
+  bool selected = false;
 
   @override
   void initState() {
@@ -35,8 +36,8 @@ class _AlarmState extends State<Alarm> {
     showDialog(
         context: context,
         builder: (_) => AlertDialog(
-              title: Text('Notification Payload'),
-              content: Text('Payload: $payload'),
+              title: Text('오늘의 알림'),
+              content: Text('~건강 챙길 시간~'),
             ));
   }
 
@@ -55,7 +56,7 @@ class _AlarmState extends State<Alarm> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            //시간 선택 스피너 
+            //시간 선택 스피너
             hourMinuteSecond(),
 
             //선택한 시간 확인
@@ -63,38 +64,50 @@ class _AlarmState extends State<Alarm> {
               margin: EdgeInsets.symmetric(vertical: 20),
               child: new Text(
                 _dateTime.hour.toString().padLeft(2, '0') +
-                    '시 ' +
+                    '시  ' +
                     _dateTime.minute.toString().padLeft(2, '0') +
-                    '분' +
+                    '분  ' +
                     _dateTime.second.toString().padLeft(2, '0') +
                     '초',
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
             Text("후에 알림"),
-            SizedBox(height: 30,),
+            SizedBox(
+              height: 30,
+            ),
 
-            //알림 예약 버튼
-            ButtonTheme(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              minWidth: 180.0,
-              height: 50.0,
-              child: MaterialButton(
-                color: const Color(0xffffb1b1),
-                child: const Text(
-                  '알림 예약',
-                  style: TextStyle(fontSize: 18.0, color: Colors.white),
-                ),
-                onPressed: () {
+            //애니메이션 버튼
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selected = !selected;
                   _dailyAtTimeNotification();
-                },
+                });
+              },
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30), //모서리를 둥글게
+            border: Border.all(color: Colors.black12, width: 3)),
+                  child: AnimatedDefaultTextStyle(
+                    duration: const Duration(milliseconds: 300),
+                    style: TextStyle(
+                      fontSize: 50.0,
+                      color: selected ? Color.fromARGB(255, 193, 224, 151) : Color.fromARGB(255, 157, 178, 214),
+                      fontWeight: selected ? FontWeight.bold : FontWeight.bold,
+                    ),
+                    child: Text('알림 예약'),
+                  ),
+                ),
               ),
             ),
+
+            
           ],
         ),
-      ), 
+      ),
     );
   }
 
@@ -120,7 +133,7 @@ class _AlarmState extends State<Alarm> {
     await _flutterLocalNotificationsPlugin!.zonedSchedule(
         123,
         '오늘의 알림',
-      '~건강 챙길 시간~',
+        '~건강 챙길 시간~',
         tz.TZDateTime.now(tz.local).add(Duration(
             hours: _dateTime.hour,
             minutes: _dateTime.minute,
